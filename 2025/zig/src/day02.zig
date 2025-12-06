6e39642ab3f7a31daef97c3f918bfe386bb8f63a
a2not <31874975+a2not@users.noreply.github.com>
2025-12-06 17:16:26 +0900

    feat(day02): refactor range type and optimize logic
    Refactored the range struct to use u64 for `from` and `to` fields, improving
    type safety and performance. Updated parsing and logic in part1 and part2 to
    avoid string conversions and regex, resulting in faster execution. Switched
    tests to use ArenaAllocator for better memory management and enabled part2
    tests. Also updated main.zig to include day02 tests.
    
    Signed-off-by: a2not <31874975+a2not@users.noreply.github.com>

--- 2025/zig/src/day02.zig
+++ 2025/zig/src/day02.zig
@@ -4,8 +4,8 @@ const Allocator = std.mem.Allocator;
 const input = @embedFile("day02_input.txt");
 
 const range = struct {
-    from: i64,
-    to: i64,
+    from: u64,
+    to: u64,
 };
 
 const Context = struct {
@@ -13,11 +13,11 @@ const Context = struct {
     ranges: []range,
 };
 
-fn parse(allocator: Allocator) !Context {
+fn parse(allocator: Allocator, inputStr: []const u8) !Context {
     var ranges = try std.ArrayList(range).initCapacity(allocator, 10000);
     errdefer ranges.deinit(allocator);
 
-    var it = std.mem.tokenizeScalar(u8, input, ',');
+    var it = std.mem.tokenizeScalar(u8, inputStr, ',');
     while (it.next()) |line| {
         if (line.len == 0) continue;
 
@@ -25,8 +25,8 @@ fn parse(allocator: Allocator) !Context {
         const fromStr = nums.next() orelse unreachable;
         const toStr = nums.next() orelse unreachable;
         const trimmedToStr = std.mem.trim(u8, toStr, " \n\r\t");
-        const from = try std.fmt.parseInt(i64, fromStr, 10);
-        const to = try std.fmt.parseInt(i64, trimmedToStr, 10);
+        const from = try std.fmt.parseInt(u64, fromStr, 10);
+        const to = try std.fmt.parseInt(u64, trimmedToStr, 10);
         try ranges.append(allocator, range{ .from = from, .to = to });
     }
 
@@ -38,92 +38,77 @@ fn parse(allocator: Allocator) !Context {
     };
 }
 
-pub fn part1(ctx: Context) !i64 {
-    var result: i64 = 0;
+pub fn part1(ctx: Context) !u64 {
+    var result: u64 = 0;
 
+    const maximum_pat: u64 = std.math.pow(u64, 10, 9);
     for (ctx.ranges) |r| {
         const from: usize = @intCast(r.from);
         const to: usize = @intCast(r.to);
         for (from..(to + 1)) |v| {
-            std.debug.print("Checking value: {d}", .{v});
-            const valueStr: []u8 = try std.fmt.allocPrint(ctx.allocator, "{d}", .{v});
-            defer ctx.allocator.free(valueStr);
+            const value: u64 = @intCast(v);
+            var pot: u64 = 10;
+            while (pot <= maximum_pat) : (pot *= 10) {
+                const pattern = @mod(value, pot);
 
-            const len = valueStr.len;
-            if (@mod(len, 2) != 0) {
-                continue;
-            }
+                const leading_zero = @divFloor(pattern, @divFloor(pot, 10)) == 0;
+                if (leading_zero) continue;
 
-            const left = valueStr[0..(len / 2)];
-            const right = valueStr[(len / 2)..len];
-            if (std.mem.eql(u8, left, right)) {
-                result += @intCast(v);
+                const shifted = @divFloor(value, pot);
+
+                if (pattern == shifted) {
+                    result += value;
+                    break;
+                }
             }
         }
     }
     return result;
 }
 
-const Regex = @import("advent_of_code").regex.Regex;
+pub fn part2(ctx: Context) !u64 {
+    var result: u64 = 0;
 
-pub fn part2(ctx: Context) !i64 {
-    var result: i64 = 0;
-
-    const pattern = "^(.+)\\1+$";
-    var re = try Regex.compile(ctx.allocator, pattern);
-    defer re.deinit();
-
+    const maximum_pat: u64 = std.math.pow(u64, 10, 9);
     for (ctx.ranges) |r| {
         const from: usize = @intCast(r.from);
         const to: usize = @intCast(r.to);
         for (from..(to + 1)) |v| {
-            const valueStr: []u8 = try std.fmt.allocPrint(ctx.allocator, "{d}", .{v});
-            defer ctx.allocator.free(valueStr);
+            var pot: u64 = 10;
+            while (pot <= maximum_pat) : (pot *= 10) {
+                var value: u64 = @intCast(v);
+                const pattern = @mod(value, pot);
 
-            if (try re.isMatch(valueStr)) {
-                result += @intCast(v);
-            } else {
-                std.debug.print("No match for value: {d}\n", .{v});
+                const leading_zero = @divFloor(pattern, @divFloor(pot, 10)) == 0;
+                if (leading_zero) continue;
+
+                value = @divFloor(value, pot);
+                if (value == 0) continue; // any value would repeat once
+
+                while (@mod(value, pot) == pattern) {
+                    value = @divFloor(value, pot);
+                }
+
+                if (value == 0) {
+                    result += @intCast(v);
+                    break;
+                }
             }
         }
     }
     return result;
 }
 
-const exampleRanges = [_]range{
-    .{ .from = 11, .to = 22 },
-    .{ .from = 95, .to = 115 },
-    .{ .from = 998, .to = 1012 },
-    .{ .from = 1188511880, .to = 1188511890 },
-    .{ .from = 222220, .to = 222224 },
-    .{ .from = 1698522, .to = 1698528 },
-    .{ .from = 446443, .to = 446449 },
-    .{ .from = 38593856, .to = 38593862 },
-    .{ .from = 565653, .to = 565659 },
-    .{ .from = 824824821, .to = 824824827 },
-    .{ .from = 2121212118, .to = 2121212124 },
-    // 11-22,
-    // 95-115,
-    // 998-1012,
-    // 1188511880-1188511890,
-    // 222220-222224,
-    // 1698522-1698528,
-    // 446443-446449,
-    // 38593856-38593862,
-    // 565653-565659,
-    // 824824821-824824827,
-    // 2121212118-2121212124
-};
+const example =
+    \\11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+;
 
 test "part1 example" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
-    const ctx = Context{
-        .allocator = allocator,
-        .ranges = @constCast(exampleRanges[0..]),
-    };
+    const ctx = try parse(allocator, example);
 
     const result = try part1(ctx);
     std.debug.print("Day 02 Part 1 Example Result: {}\n", .{result});
@@ -131,46 +116,37 @@ test "part1 example" {
 }
 
 test "part1" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
-    const ctx = try parse(allocator);
-    defer {
-        allocator.free(ctx.ranges);
-    }
+    const ctx = try parse(allocator, input);
 
     const result = try part1(ctx);
     std.debug.print("Day 02 Part 1 Result: {}\n", .{result});
     try std.testing.expectEqual(37314786486, result);
 }
 
-// test "part2 example" {
-//     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-//     defer _ = gpa.deinit();
-//     const allocator = gpa.allocator();
-//
-//     const ctx = Context{
-//         .allocator = allocator,
-//         .ranges = @constCast(exampleRanges[0..]),
-//     };
-//
-//     const result = try part2(ctx);
-//     std.debug.print("Day 02 Part 2 Example Result: {}\n", .{result});
-//     try std.testing.expectEqual(4174379265, result);
-// }
-//
-// test "part2" {
-//     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-//     defer _ = gpa.deinit();
-//     const allocator = gpa.allocator();
-//
-//     const ctx = try parse(allocator);
-//     defer {
-//         allocator.free(ctx.ranges);
-//     }
-//
-//     const result = try part2(ctx);
-//     std.debug.print("Day 02 Part 2 Result: {}\n", .{result});
-//     try std.testing.expectEqual(47477053982, result);
-// }
+test "part2 example" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const ctx = try parse(allocator, example);
+
+    const result = try part2(ctx);
+    std.debug.print("Day 02 Part 2 Example Result: {}\n", .{result});
+    try std.testing.expectEqual(4174379265, result);
+}
+
+test "part2" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const ctx = try parse(allocator, input);
+
+    const result = try part2(ctx);
+    std.debug.print("Day 02 Part 2 Result: {}\n", .{result});
+    try std.testing.expectEqual(47477053982, result);
+}
